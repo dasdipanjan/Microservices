97a7f98c19b2e57fd0124b6a0f1df8c8c28e0618
DIPANJAN DAS <das.dipanjan@hotmail.com>
2022-11-23 01:11:58 +0530

    Sping Microservice configuration added.

--- spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/SpringBootDemoApplication.java
@@ -8,5 +8,6 @@ public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.println("SpringBoot demo application has been started. Developed By Dipanjan");
 	}
 }
